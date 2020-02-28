82bf563e4fd7d38d2a15e29d9b0923338f7b1f0f
Jeremy Kale Padot <jeremy@padot.us>
2020-02-28 10:44:08 -0800

    add properties to pastry class

--- Bakery.cs
+++ Bakery.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 namespace Bakery.Models
 {
 
-
   public class Bread
   {
     private string _breadName;
@@ -32,10 +31,37 @@ namespace Bakery.Models
       _breadPrice = newBreadPrice;
     }
 
+
       // public class Bread ends below
   }
+
   public class Pastry
   {
+    private string _pastryName;
+    private int _pastryPrice;
+
+    public Pastry(string pastryName, int pastryPrice)
+    {
+      _pastryName = pastryName;
+      _pastryPrice = pastryPrice;
+    }
+    public string GetPastryName()
+    {
+      return _pastryName;
+    }
+    public int GetPastryPrice()
+    {
+      return _pastryPrice;
+    }
+    public void SetPastryName(string newPastryName)
+    {
+      _pastryName = newPastryName;
+    }
+    public void SetPastryPrice(int newPastryPrice)
+    {
+      _pastryPrice = newPastryPrice;
+    }
+
     // public class Pastry ends below
   }
 // namespace ends below
