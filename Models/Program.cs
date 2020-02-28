9ff1fbf62a222c7dcc5dd21d21dea4d819163507
Jeremy Kale Padot <jeremy@padot.us>
2020-02-28 12:04:39 -0800

    add list for bread and pastry class and display name/price

--- Models/Program.cs
+++ Models/Program.cs
@@ -8,12 +8,21 @@ public class Program
   public static void Main()
   {
     Bread baguette = new Bread("Fresh Baguette",5);
-    List<Bread> Breads = new List<Bread>() {baguette};
-    Console.WriteLine("Welcome Message");
+    Bread sourdough = new Bread ("Sourdough Loaf",5);
+    Pastry sweetroll = new Pastry ("Sweet Roll", 2);
+    List<Bread> Breads = new List<Bread>() {baguette, sourdough};
+    List<Pastry> Pastrys = new List<Pastry>() {sweetroll};
+    Console.WriteLine("Welcome to Haru's Bakery");
+    Console.WriteLine("Take a look at our menu");
     foreach (Bread cart in Breads)
     {
       Console.WriteLine(cart.GetBreadName());
       Console.WriteLine(cart.GetBreadPrice());
+    }
+    foreach (Pastry cart in Pastrys)
+    {
+      Console.WriteLine(cart.GetPastryName());
+      Console.WriteLine(cart.GetPastryPrice());
     }
     
   
