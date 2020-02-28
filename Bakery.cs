48084883f8e582d19f0cc0cc4cbda6af45fde343
Jeremy Kale Padot <jeremy@padot.us>
2020-02-28 10:35:27 -0800

    add properties to bread class

--- Bakery.cs
+++ Bakery.cs
@@ -7,7 +7,36 @@ namespace Bakery.Models
 
   public class Bread
   {
-    
+    private string _breadName;
+    private int _breadPrice;
+
+    public Bread(string breadName, int breadPrice)
+    {
+      _breadName = breadName;
+      _breadPrice = breadPrice;
+    }
+    public string GetBreadName()
+    {
+      return _breadName;
+    }
+    public int GetBreadName()
+    {
+      return _breadName;
+    }
+    public void SetBreadname(string newBreadName)
+    {
+      _breadName = newBreadName;
+    }
+    public void SetBreadPrice(int newBreadPrice)
+    {
+      _breadPrice = newBreadPrice;
+    }
+
+      // public class Bread ends below
+  }
+  public class Pastry
+  {
+    // public class Pastry ends below
   }
 // namespace ends below
 }
